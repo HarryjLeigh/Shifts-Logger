42add81313beecdf725130237257b4308192edc0
harryL <harryleigh@hotmail.co.uk>
2024-12-04 20:42:06 +0000

    Project Finished. Added ReadMe File.

--- ShiftsLogger/API/Data/ShiftsDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-using ShiftsLogger.API.Models;
-
-namespace ShiftsLogger.API.Data;
-
-public class ShiftsDbContext(DbContextOptions<ShiftsDbContext> options) : DbContext(options)
-{
-    public DbSet<Shift> Shifts { get; set; }
-}
--- ShiftsLogger/API/Models/Shift.cs
+++
@@ -1,9 +0,0 @@
-namespace ShiftsLogger.API.Models;
-
-public class Shift
-{
-    public int Id { get; set; }
-    public DateTime Date { get; set; }
-    public int Hours { get; set; }
-    public int AmountOfTime { get; set; }
-}
